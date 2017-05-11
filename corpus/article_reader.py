9db35b17f14aef77ff702e48df055e7c5414fdac
Hun Choi <choihun121@gmail.com>
2017-05-11 17:06:22 -0400

    graphing implemented

--- corpus/article_reader.py
+++ corpus/article_reader.py
@@ -1,9 +1,15 @@
 # usage: python article_reader.py json/nytimes_trump.json
+from collections import OrderedDict
+import datetime
 import json
+import matplotlib.pyplot as plt
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk import tokenize
+import numpy as np
+from scipy import stats
 from spacy.en import English
 import sys
+import time
 
 # take in news outlet json file
 file_name = sys.argv[1]
@@ -15,8 +21,13 @@ parser = English()
 
 # look at each sentence and judge the overall sentiment
 sid = SentimentIntensityAnalyzer()
-total_sentiment = 0.0
-total_count = 0
+
+# plotting axes
+x = []
+x_ticks = []
+y = []
+daily_count = {}
+daily_average = {}
 
 for news_page in outlet:
     page_text = news_page['newspaper_article_text']
@@ -33,18 +44,42 @@ for news_page in outlet:
         parsedData = parser(sentence)
         sub_toks = [tok for tok in parsedData if (tok.dep_ == "nsubj") ]
         for tok in sub_toks:
-            if tok.orth_ == "Trump":
+            if "Trump" in tok.orth_ or "POTUS" in tok.orth_:
                 #print sentence
                 ss = sid.polarity_scores(sentence)
                 average_sentiment += ss['compound']
-                total_sentiment += ss['compound']
                 count += 1
-                total_count += 1
-                # prints out the different sentiments 
-                # for key in sorted(ss):
-                #     print "{0}: {1}, ".format(key, ss[key]),
-                # print "\n"
     if count is not 0:
-        print news_page['canonicalUrl']
-        print "Article sentiment average: " + str(average_sentiment/count)
-# print "Outlet sentiment average: " + str(total_sentiment/total_count)
+        date = datetime.datetime.fromtimestamp(news_page['published']/1000).strftime('%Y/%m/%d')
+        if date not in daily_count:
+            daily_count[date] = 1
+            daily_average[date] = average_sentiment/count
+        else:
+            daily_count[date] += 1
+            daily_average[date] += (average_sentiment/count)
+
+# sort by date order for convenience
+ordered_averages = OrderedDict(sorted(daily_average.items()))
+for date,sentiments in ordered_averages.iteritems():
+    sentiments /= daily_count[date]
+    x.append(time.mktime(datetime.datetime.strptime(date, "%Y/%m/%d").timetuple()))
+    y.append(sentiments)
+    x_ticks.append(date)
+
+# set up matplotlib
+np_x = np.array(x)
+np_y = np.array(y)
+np_x_ticks = np.array(x_ticks)
+fig = plt.figure(1, figsize=(20,10))
+plt.xticks(x, x_ticks, rotation=90)
+title_index = file_name.find("_trump.json")
+title = file_name[5:title_index] + " sentiment analysis"
+plt.title(title)
+
+# linear best fit
+slope, intercept, r_value, p_value, std_err = stats.linregress(np_x, np_y)
+line = slope*np_x+intercept
+
+# plot
+plt.plot(x, y, "o--", x, line)
+plt.show()
